b97533aadc7837cf21f0687357063ff5414f8a0e
suatcg <suat-can_24@hotmail.com>
2022-11-14 12:04:49 +0300

    eslint-error-fixed

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -2,57 +2,57 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  updateLoggedInUserFollowing,
-  updateFollowedUserFollowers,
+	updateLoggedInUserFollowing,
+	updateFollowedUserFollowers,
 } from '../../services/firebase';
 
 export default function SuggestedProfile({
-  profileDocId,
-  username,
-  profileId,
-  userId,
-  loggedInUserDocId,
+	profileDocId,
+	username,
+	profileId,
+	userId,
+	loggedInUserDocId,
 }) {
-  const [followed, setFollowed] = useState(false);
+	const [followed, setFollowed] = useState(false);
 
-  async function handleFolloweUser() {
-    setFollowed(true);
+	async function handleFolloweUser() {
+		setFollowed(true);
 
-    // firebase: create 2 services (functions)
-    // update the followind array of the logged in user (in this case, my profile)
-    await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false);
-    // update the followers array of the user who has been followed
-    await updateFollowedUserFollowers(profileDocId, userId, false);
-  }
+		// firebase: create 2 services (functions)
+		// update the followind array of the logged in user (in this case, my profile)
+		await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false);
+		// update the followers array of the user who has been followed
+		await updateFollowedUserFollowers(profileDocId, userId, false);
+	}
 
-  return !followed ? (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center justify-between">
-        <img
-          className="rounded-full w-8 flex mr-3"
-          src={`/images/avatars/${username}.jpg`}
-          alt=""
-        />
-        <Link to={`/p/${username}`}>
-          <p className="font-bold text-sm">{username}</p>
-        </Link>
-      </div>
+	return !followed ? (
+		<div className="flex items-center justify-between">
+			<div className="flex items-center justify-between">
+				<img
+					className="rounded-full w-8 flex mr-3"
+					src={`/images/avatars/${username}.jpg`}
+					alt=""
+				/>
+				<Link to={`/p/${username}`}>
+					<p className="font-bold text-sm">{username}</p>
+				</Link>
+			</div>
 
-      <button
-        className="text-sm font-bold text-blue-medium"
-        type="button"
-        onClick={handleFolloweUser}
-      >
-        Follow
-      </button>
-    </div>
-  ) : null;
+			<button
+				className="text-sm font-bold text-blue-medium"
+				type="button"
+				onClick={handleFolloweUser}
+			>
+				Follow
+			</button>
+		</div>
+	) : null;
 }
 
-SuggestedProfile.proptypes = {
-  profileDocId: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  profileId: PropTypes.string.isRequired,
-  userId: PropTypes.string.isRequired,
-  loggedInUserDocId: PropTypes.string.isRequired,
+SuggestedProfile.propTypes = {
+	profileDocId: PropTypes.string.isRequired,
+	username: PropTypes.string.isRequired,
+	profileId: PropTypes.string.isRequired,
+	userId: PropTypes.string.isRequired,
+	loggedInUserDocId: PropTypes.string.isRequired,
 };
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,3 +1,4 @@
+import FirebaseContext from '../context/firebase';
 import { useContext, useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 
@@ -5,141 +6,139 @@ import * as ROUTES from '../constants/routes';
 import { doesUsernameExist } from '../services/firebase';
 
 const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
 };
 
-import FirebaseContext from '../context/firebase';
-
 export default function Signup() {
-  const history = useHistory();
-  const { firebase } = useContext(FirebaseContext);
+	const history = useHistory();
+	const { firebase } = useContext(FirebaseContext);
 
-  const [username, setUsername] = useState('');
-  const [fullName, setFullName] = useState('');
+	const [username, setUsername] = useState('');
+	const [fullName, setFullName] = useState('');
 
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
+	const [emailAddress, setEmailAddress] = useState('');
+	const [password, setPassword] = useState('');
 
-  const [error, setError] = useState('');
-  const isInvalid = password === '' || !validateEmail(emailAddress);
+	const [error, setError] = useState('');
+	const isInvalid = password === '' || !validateEmail(emailAddress);
 
-  useEffect(() => {
-    document.title = 'Sign Up - Instagram';
-  }, []);
+	useEffect(() => {
+		document.title = 'Sign Up - Instagram';
+	}, []);
 
-  const handleSignup = async (event) => {
-    event.preventDefault();
+	const handleSignup = async (event) => {
+		event.preventDefault();
 
-    const usernameExist = await doesUsernameExist(username);
-    console.log('username exist', usernameExist);
+		const usernameExist = await doesUsernameExist(username);
+		console.log('username exist', usernameExist);
 
-    if (!usernameExist.length) {
-      try {
-        const createdUserResult = await firebase
-          .auth()
-          .createUserWithEmailAndPassword(emailAddress, password);
+		if (!usernameExist.length) {
+			try {
+				const createdUserResult = await firebase
+					.auth()
+					.createUserWithEmailAndPassword(emailAddress, password);
 
-        // authentication
-        // -> emailAddress & password & username(displayName)
-        await createdUserResult.user.updateProfile({ displayName: username });
+				// authentication
+				// -> emailAddress & password & username(displayName)
+				await createdUserResult.user.updateProfile({ displayName: username });
 
-        // firebase user collection (create a document)
-        await firebase.firestore().collection('users').add({
-          userId: createdUserResult.user.uid,
-          username: username.toLocaleLowerCase(),
-          fullName,
-          emailAddress: emailAddress.toLocaleLowerCase(),
-          following: [],
-          dataCreated: Date.now(),
-        });
+				// firebase user collection (create a document)
+				await firebase.firestore().collection('users').add({
+					userId: createdUserResult.user.uid,
+					username: username.toLocaleLowerCase(),
+					fullName,
+					emailAddress: emailAddress.toLocaleLowerCase(),
+					following: [],
+					dataCreated: Date.now(),
+				});
 
-        history.push(ROUTES.DASHBOARD);
-      } catch (error) {
-        setFullName('');
-        setEmailAddress('');
-        setPassword('');
-        setError(error.message);
-      }
-    } else {
-      setError('That username is already taken , please try another.');
-    }
-  };
-  return (
-    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
-        <img
-          src="/images/iphone-with-profile.jpg"
-          alt="iPhone with Instagram app"
-        />
-      </div>
-      <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          <h1 className="flex justify-center w-full">
-            <img
-              src="/images/logo.png"
-              alt="Instagram"
-              className="mt-2 w-6/12 mb-4"
-            />
-          </h1>
-          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+				history.push(ROUTES.DASHBOARD);
+			} catch (error) {
+				setFullName('');
+				setEmailAddress('');
+				setPassword('');
+				setError(error.message);
+			}
+		} else {
+			setError('That username is already taken , please try another.');
+		}
+	};
+	return (
+		<div className="container flex mx-auto max-w-screen-md items-center h-screen">
+			<div className="flex w-3/5">
+				<img
+					src="/images/iphone-with-profile.jpg"
+					alt="iPhone with Instagram app"
+				/>
+			</div>
+			<div className="flex flex-col w-2/5">
+				<div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+					<h1 className="flex justify-center w-full">
+						<img
+							src="/images/logo.png"
+							alt="Instagram"
+							className="mt-2 w-6/12 mb-4"
+						/>
+					</h1>
+					{error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleSignup} method="POST">
-            <input
-              aria-label="Enter your username"
-              type="text"
-              placeholder="Username"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-            />
-            <input
-              aria-label="Enter your full name"
-              type="text"
-              placeholder="Full Name"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={(e) => setFullName(e.target.value)}
-              value={fullName}
-            />
-            <input
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={(e) => setEmailAddress(e.target.value)}
-              value={emailAddress}
-            />
-            <input
-              aria-label="Enter your password"
-              type="password"
-              placeholder="Email password"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-            />
-            <button
-              disabled={isInvalid}
-              type="submit"
-              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
-                isInvalid && 'opacity-50'
-              }`}
-            >
-              Sign Up
-            </button>
-          </form>
-        </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
-          <p className="text-sm">
-            Have an account?{' '}
-            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
-              Login
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+					<form onSubmit={handleSignup} method="POST">
+						<input
+							aria-label="Enter your username"
+							type="text"
+							placeholder="Username"
+							className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+							onChange={(e) => setUsername(e.target.value)}
+							value={username}
+						/>
+						<input
+							aria-label="Enter your full name"
+							type="text"
+							placeholder="Full Name"
+							className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+							onChange={(e) => setFullName(e.target.value)}
+							value={fullName}
+						/>
+						<input
+							aria-label="Enter your email address"
+							type="text"
+							placeholder="Email address"
+							className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+							onChange={(e) => setEmailAddress(e.target.value)}
+							value={emailAddress}
+						/>
+						<input
+							aria-label="Enter your password"
+							type="password"
+							placeholder="Email password"
+							className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						/>
+						<button
+							disabled={isInvalid}
+							type="submit"
+							className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+								isInvalid && 'opacity-50'
+							}`}
+						>
+							Sign Up
+						</button>
+					</form>
+				</div>
+				<div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+					<p className="text-sm">
+						Have an account?{' '}
+						<Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+							Login
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
