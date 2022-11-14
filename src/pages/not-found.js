1151f04d1eeedc6bba8aa69520f003c1e9248b46
suatcg <suat-can_24@hotmail.com>
2022-11-14 11:42:09 +0300

    production-ready

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
+import Header from '../components/header';
 
 export default function NotFound() {
-  useEffect(() => {
-    document.title = 'Not Found!';
-  }, []);
+	useEffect(() => {
+		document.title = 'Not Found!';
+	}, []);
 
-  return (
-    <div className="bg-gray-background">
-      <div className="mx-auto max-w-screen-lg">
-        <p className="text-center text-2xl">Not Found!</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-gray-background">
+			<Header />
+			<div className="mx-auto max-w-screen-lg">
+				<p className="text-center text-2xl">Not Found!</p>
+			</div>
+		</div>
+	);
 }
