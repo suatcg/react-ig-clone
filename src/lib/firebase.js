39a7cf9bdf3a37fe73e295aec6638cb6cb173c36
suatcg <suat-can_24@hotmail.com>
2022-11-14 11:50:43 +0300

    lastly

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import 'firebase/compat/auth';
 
 // here i want to import seed file
 const config = {
-  apiKey: 'AIzaSyBKRkU69LQkWF29_qrGATKIrJEmLCDuHYY',
-  authDomain: 'instagram-clone-cde75.firebaseapp.com',
-  projectId: 'instagram-clone-cde75',
-  storageBucket: 'instagram-clone-cde75.appspot.com',
-  messagingSenderId: '182948873784',
-  appId: '1:182948873784:web:6c59cc79f8e2bb455748cd',
+	apiKey: 'AIzaSyBKRkU69LQkWF29_qrGATKIrJEmLCDuHYY',
+	authDomain: 'instagram-clone-cde75.firebaseapp.com',
+	projectId: 'instagram-clone-cde75',
+	storageBucket: 'instagram-clone-cde75.appspot.com',
+	messagingSenderId: '182948873784',
+	appId: '1:182948873784:web:6c59cc79f8e2bb455748cd',
 };
 
 const firebase = Firebase.initializeApp(config);
